92825c6233c3a48d2fdbe3a0ece46f595528e455
Tal Ekroni <talekroni01@gmail.com>
2022-01-15 17:19:37 +0200

    getting there

---
+++ src/cmps/AppHeader.jsx
@@ -0,0 +1,8 @@
+export function AppHeader(){
+    return(
+        <div className="appHeader flex justify-center column align-center ">
+            <h2>Tic Tac Toe </h2>
+            <small>Online biatch</small>
+        </div>
+    )
+}
--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,5 +1,19 @@
-export function HomePage(){
+import React from "react";
+
+export class HomePage extends React.Component {
+  state = {
+    roomId: "",
+  };
+  handleChange = ({ target }) => {
+    this.setState({ roomId: target.value }, console.log(this.state));
+  };
+  render() {
+    const { roomId } = this.state;
     return (
-        <div>hi home page biatch</div>
-    )
+      <div className="home-page flex column justify-center align-center">
+        <input type="text" value={roomId} onChange={this.handleChange} />
+        <button>Enter a room</button>
+      </div>
+    );
+  }
 }
