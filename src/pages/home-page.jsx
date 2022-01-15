26e2c75beaf1483ee0d3dd0133329b8af4e9071d
Tal Ekroni <talekroni01@gmail.com>
2022-01-15 17:22:06 +0200

    getting there

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { socketService } from "../services/socket.service";
 
 export class HomePage extends React.Component {
   state = {
     roomId: "",
   };
+  componentDidMount() {
+    socketService.setup();
+  }
   handleChange = ({ target }) => {
     this.setState({ roomId: target.value }, console.log(this.state));
   };
